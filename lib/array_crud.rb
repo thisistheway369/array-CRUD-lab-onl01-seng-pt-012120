ea9f82c5ac210740f37db7a0c74badaec27b6e40
thisistheway369 <thisistheway369@icloud.com>
2020-01-29 05:49:28 -0800

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,49 @@
-def create_an_empty_array
+def 
+    create_an_empty_array
+    [ ]
+  end
   
-end
+  def create_an_array
+    cats = ["c1","c2","c3","c4"]
+  end
+  
+  def add_element_to_end_of_array(array, element)
+    cats = ["c1","c2","c3","c4"]
+    array << element
+   
+    
+  end
+  
+  def add_element_to_start_of_array(array, element)
+    cats = ["c1","c2","c3","c4"]
+    array.unshift(element) 
+    
+  end
+  
+  def remove_element_from_end_of_array(array)
+    cats = ["c1","c2","c3","c4"]
+     array.pop
+    
+  end
+  
+  def remove_element_from_start_of_array(array)
+    cats = ["c1","c2","c3","c4"]
+    array.shift
+   
+  end
+  
+  def retrieve_element_from_index(array, index_number)
+    
+      array[index_number]
+  end
+  def retrieve_first_element_from_array(array)
+   
+    cats = ["c1","c2","c3","c4"]
+   array[0]
+  end
+  
+  def retrieve_last_element_from_array(array)
+    array[-1]
+  end
+  
 
-def create_an_array
-  
-end
-
-def add_element_to_end_of_array(array, element)
-  
-end
-
-def add_element_to_start_of_array(array, element)
-  
-end
-
-def remove_element_from_end_of_array(array)
-  
-end
-
-def remove_element_from_start_of_array(array)
-  
-end
-
-def retrieve_element_from_index(array, index_number)
-  
-end
-
-def retrieve_first_element_from_array(array)
-  
-end
-
-def retrieve_last_element_from_array(array)
-  
-end
